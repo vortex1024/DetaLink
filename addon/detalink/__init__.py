505b668b238659b205edf3e5560a8d6f69cca9c7
vortex <vortex37@gmail.com>
2021-01-02 21:36:23 +0200

    first commit

---
+++ addon/detalink/__init__.py
@@ -0,0 +1,65 @@
+import globalPluginHandler
+import scriptHandler
+import ui
+import api
+import textInfos
+import re
+import logHandler
+import os.path
+import subprocess
+import sys
+impPath = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(impPath)
+import urllib.request
+import HTMLParser
+from NVDAObjects.UIA import UIA
+
+htmlParser=HTMLParser.HTMLParser()
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	vlcProc=None
+	
+	def getFocusedLink(self):
+		obj=api.getFocusObject()
+		if isinstance(obj, UIA):
+			link=obj.name
+		else:
+			try:
+				link=obj.value
+			except:
+				pass
+		return link
+
+	def getTitle(self, link):
+		global htmlParser
+		request=urllib.request.Request(link)
+		request.add_header('user-agent', 'mozilla 5.10')
+		#request.add_header("Range", "bytes=0-1024")
+		html=urllib.request.urlopen(request).read().decode('utf-8')
+		title=htmlParser.unescape(re.search('(?<=<title>).+?(?=</title>)', html, re.DOTALL).group().strip())
+		
+		return title 
+
+	def script_linkTitle(self, gesture):
+		link=self.getFocusedLink()
+		self.vlcProc.terminate() if self.vlcProc is not None else None
+		if link is None or link.startswith("http")==False:
+			selection=api.getCaretObject().makeTextInfo(textInfos.POSITION_SELECTION).text
+			if selection.startswith("http"):
+				link=selection
+
+			else:
+				ui.message("mot a link")
+				
+				return
+		if(re.match(".*.(aac|mp4).*", link)):
+			self.vlcProc=subprocess.Popen(["c:/program files/VideoLAN/vlc/vlc.exe","-Idummy", link])
+			
+
+		else:
+			title=self.getTitle(link)
+			ui.message(title)
+
+	__gestures={
+		"kb:NVDA+alt+q": "linkTitle"
+	}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# DetaLink
+## what it does
+It speaks the title of the page a link goes to, without actually opening it. Remember those bit.ly links? Just press nvda+alt+q on them, and you'll find out what they actually link to, therefore  you'll be able to decide if they are worth opening. The same with youTube clips.
+Also, if the link is an mp4 or aac audio, it plays it without opening a player. To stop the audio, just focus a non-link element and press nnvda+alt+q again. It is not possible to restart the playback from where you stopped, this was designed only for small files.
+## how to build
+Just zip the contents of the addon folder and change the extension to .nvda-addon
